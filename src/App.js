274a81f7ef426057c0319c5aaf155625a39b43d4
Jarret Jheng Ch'ng <jarretchng02@gmail.com>
2021-01-20 15:06:19 +1100

    Add fetch code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Movie from './components/Movie';
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?api_key=2bf56b4e029ff5fca38a61a671156184&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=" + FEATURED_API + "&query="
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="movie-container">
+      <header>
+        <input type="text" placehoder="Search..." />s
       </header>
+      {movies.length > 0 && movies.map(movie => (
+        <Movie key={movie.id} {...movie} />
+      ))}
     </div>
   );
 }
